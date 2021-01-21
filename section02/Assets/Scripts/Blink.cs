edce70a1f7fe4af7292d7a0bf6bc8142d551f7b1
junjunantolin <antolin_e@yahoo.com>
2021-01-21 14:28:03 +0800

    first commit

---
+++ section02/Assets/Scripts/Blink.cs
@@ -0,0 +1,35 @@
+﻿ using UnityEngine;
+ using System.Collections;
+ using UnityEngine.UI;
+ using UnityEngine.EventSystems;
+ 
+ public class Blink : MonoBehaviour {
+ 
+  public float timer;
+     public GameObject Tap;
+     void exit()
+     {
+         if (Input.touchCount > 0)
+         {
+           Tap.SetActive(false);
+         }
+     
+ 
+         }
+ 
+     void Update()
+     {
+         timer = timer + Time.deltaTime;
+         if(timer >= 0.5)
+         {
+                 GetComponent<Text>().enabled = true;
+         }
+         if(timer >= 1)
+         {
+                 GetComponent<Text>().enabled = false;
+                 timer = 0;
+         }
+ 
+     }
+ 
+ }
---
+++ section02/Assets/Scripts/PhysicsEngine.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsEngine : MonoBehaviour
+{
+    public float mass;
+    public GameObject StatusGood;
+    public GameObject StatusBad;
+    public Vector3 velocityVector; //average velocity this fixed update()
+    public Vector3 netForceVector;
+    public List<Vector3> forceVectorList = new List<Vector3>(); 
+
+    void Start()
+    {
+        StatusBad.SetActive(false);
+        StatusGood.SetActive(true);
+    }
+
+    
+    void FixedUpdate()
+    {
+        AddForces();
+        UpdateVelocity();
+        if (netForceVector == Vector3.zero)
+        {
+            transform.position += velocityVector * Time.deltaTime;
+            
+        }else{
+            StatusGood.SetActive(false);
+            StatusBad.SetActive(true);
+        }
+        //transform.position += vectorVelocity * Time.deltaTime;
+    }
+
+    void UpdateVelocity(){
+        Vector3 accelerationVector = netForceVector / mass;
+        velocityVector += accelerationVector * Time.deltaTime;
+    }
+
+    void AddForces(){
+        netForceVector = Vector3.zero;
+
+        foreach (Vector3 forceVector in forceVectorList){
+            netForceVector += forceVector;
+        }
+    }
+}
